64dc0609a5de5adeb493383588275eb4b92ca6ce
Mochizue-RBT <raventejada1929@gmail.com>
2025-07-20 19:42:09 +0800

    Added sidebar

---
+++ public/JS/index.js
@@ -0,0 +1,7 @@
+const sidebar = document.querySelector('.sidebar-exit');
+const body = document.querySelector('.body-content')
+
+function sideBarExit(){
+  sidebar.classList.add('remove');
+  body.classList.add('grid-hide');
+};
