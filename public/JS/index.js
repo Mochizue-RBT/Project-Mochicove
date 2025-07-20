590343763b5e9f9c8dc46d6581980e26e482cb43
Mochizue-RBT <raventejada1929@gmail.com>
2025-07-21 01:54:42 +0800

    Completed sidebar functions

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,7 +1,24 @@
 const sidebar = document.querySelector('.sidebar-exit');
-const body = document.querySelector('.body-content')
+const body = document.querySelector('.body-content');
+const sidebarHeader = document.querySelector('.sidebar-logo');
+const sidebarOpen = document.querySelector('.sidebar-open');
+const sidebarLinks = document.querySelector('.sidebar-links');
+const link = document.querySelector('link');
 
 function sideBarExit(){
   sidebar.classList.add('remove');
+  sidebarHeader.classList.add('remove');
   body.classList.add('grid-hide');
+  sidebarOpen.classList.add('open');
+  sidebarLinks.classList.add('remove');
+  link.classList.add('remove');
 };
+
+function sideBarOpen(){
+  sidebar.classList.remove('remove');
+  sidebarHeader.classList.remove('remove');
+  body.classList.remove('grid-hide');
+  sidebarOpen.classList.remove('open');
+  sidebarLinks.classList.remove('remove');
+  link.classList.remove('remove');
+}
